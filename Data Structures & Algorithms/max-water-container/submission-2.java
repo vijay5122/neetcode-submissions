cce392fe085f8198873d87f98f1ebf0b63703a44
vijay5122 <vijaysarathy94@gmail.com>
2026-05-20 13:11:44 +0200

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxArea(int[] heights) {
+    int maxArea = 0, start = 0, end = heights.length -1;
+    while(start < end) {
+        maxArea = Math.max(maxArea, (end - start) * Math.min(heights[start], heights[end]));
+        if ( heights[start] < heights[end]) {
+            start++;
+        } else {
+            end--;
+        }
+    }
+    return maxArea;    
+    }
+}
